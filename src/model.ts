fc4455971c0ae8b604a3d54c1267ce33148b1869
Edward Tanguay (windows) <edward@tanguay.info>
2023-05-12 11:14:22 +0200

    minimized

--- src/model.ts
+++ src/model.ts
@@ -1,17 +1,17 @@
 import fs from 'fs';
 import { IBook, IRawBook } from './interfaces.js';
 
-const rawBooks: IRawBook[] = JSON.parse(fs.readFileSync('./src/data/rawBooks.json', 'utf8'));
+// const rawBooks: IRawBook[] = JSON.parse(fs.readFileSync('./src/data/rawBooks.json', 'utf8'));
 
-const books: IBook[] = rawBooks.map(rawBook => {
-	return {
-		id: rawBook.id,
-		idCode: rawBook.idCode,
-		title: rawBook.title,
-		description: rawBook.description,
-		language: rawBook.language === '' ? 'english' : rawBook.language
-	}
-});
+// const books: IBook[] = rawBooks.map(rawBook => {
+// 	return {
+// 		id: rawBook.id,
+// 		idCode: rawBook.idCode,
+// 		title: rawBook.title,
+// 		description: rawBook.description,
+// 		language: rawBook.language === '' ? 'english' : rawBook.language
+// 	}
+// });
 
 export const getApiInstructions = () => {
 	return `
@@ -26,18 +26,15 @@ export const getApiInstructions = () => {
 		background-color: #333;
 	}
 </style>
-<h1>Book Site API</h1>
-<ul>
-	<li><code>/books</code> - all books</li>
-	<li><code>/books/3</code> - book with id 3</li>
-</ul>
+<h1>Jobs/Skills Site</h1>
+
 	`;
 }
 
-export const getBooks = (): IBook[] => {
-	return books;
-}
+// export const getBooks = (): IBook[] => {
+// 	return books;
+// }
 
-export const getBook = (id: number): IBook => {
-	return books.find(m => m.id === id);
-}
+// export const getBook = (id: number): IBook => {
+// 	return books.find(m => m.id === id);
+// }
